a3bded32a2e82ccfa6b7bd8578a14e5be3757537
Temitayo Elizabeth <teshytemmy@gmail.com>
2022-10-21 01:37:33 +0100

    Assessment Solution

--- Task1/test/test_tiketa_website.py
+++ Task1/test/test_tiketa_website.py
@@ -9,10 +9,10 @@ from src.pages.search_page import SearchPage
 class TiketaTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        with open('/Users/jacobaudu/Desktop/TeleSoftas/Task1/test/config.json') as config_file: 
+        with open(r'C:\Users\DELL\Downloads\TeleSoftas\Task1\test\config.json') as config_file: 
             self.config = json.load(config_file)
         if self.config.get('browser') == "chrome":
-            self.driver = webdriver.Chrome("/Users/jacobaudu/Desktop/TeleSoftas/Task1/drivers/chromedriver")
+            self.driver = webdriver.Chrome(r"C:\Users\DELL\Downloads\TeleSoftas\Task1\drivers\chromedriver")
 
     def test_detailed_search_functionality(self):
         url = self.config.get("url")
